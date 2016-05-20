bbd409f431c6a34e452b31769afff621022079ab
artur1707 <artur.laguza@gmail.com>
2016-05-20 14:02:49 +0300

    test play

--- cookbooks/wds-play/recipes/default.rb
+++ cookbooks/wds-play/recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+template '#{node['wds-play']['runscript_path']}/play-run.sh' do
+  source 'play-run.sh.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+file '/opt/install/play/#{node['wds-play']['play_ver']}' do
+  content '<html>This is a placeholder for the home page.</html>'
+  mode '0755'
+  owner 'web_admin'
+  group 'web_admin'
+end
